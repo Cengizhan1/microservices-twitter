5f85823340a3b6b9689974532c1d90b795c6c8c2
Cengizhan1 <182803014@ogr.cbu.edu.tr>
2023-12-21 16:39:35 +0300

    initial commit

---
+++ common-config/src/main/java/com/microservices/twitter/common/config/RetryConfig.java
@@ -0,0 +1,43 @@
+package com.microservices.twitter.common.config;
+
+import com.microservices.twitter.config.RetryConfigData;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
+
+
+@Configuration
+public class RetryConfig {
+
+    private final RetryConfigData retryConfigData;
+
+    public RetryConfig(RetryConfigData configData) {
+        this.retryConfigData = configData;
+    }
+
+    @Bean
+    public RetryTemplate retryTemplate() {
+        RetryTemplate retryTemplate = new RetryTemplate();
+
+        ExponentialBackOffPolicy exponentialBackOffPolicy = new ExponentialBackOffPolicy();
+        exponentialBackOffPolicy.setInitialInterval(retryConfigData.getInitialIntervalMs());
+        exponentialBackOffPolicy.setMaxInterval(retryConfigData.getMaxIntervalMs());
+        exponentialBackOffPolicy.setMultiplier(retryConfigData.getMultiplier());
+
+        retryTemplate.setBackOffPolicy(exponentialBackOffPolicy);
+
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(retryConfigData.getMaxAttempts());
+
+        retryTemplate.setRetryPolicy(simpleRetryPolicy);
+
+        return retryTemplate;
+    }
+}
+
+
+
